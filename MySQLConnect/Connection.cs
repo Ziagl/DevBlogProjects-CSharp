8e2519478710cd2e0c98cfe858b64105b4366938
Ziagl <office@ziegelwanger-edv.at>
2020-07-31 13:59:26 +0200

    added MySQL C# project

---
+++ MySQLConnect/Connection.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using MySql.Data.MySqlClient;
+
+namespace MySQLConnect
+{
+    class Connection
+    {
+        private MySqlConnection connection;
+        private string server;
+        private string database;
+        private string uid;
+        private string password;
+
+        //Constructor
+        public Connection()
+        {
+            Initialize();
+        }
+
+        //Initialize values
+        private void Initialize()
+        {
+            server = "ADD_YOUR_SERVER_URL_OR_IP_ADDRESS_HERE";
+            database = "ADD_YOUR_DATABASE_NAME_HERE";
+            uid = "ADD_YOUR_USERNAME_HERE";
+            password = "ADD_YOUR_PASSWORD_HERE";
+            string connectionString;
+            connectionString = "SERVER=" + server + ";" + "DATABASE=" +
+            database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+
+            connection = new MySqlConnection(connectionString);
+        }
+
+        //open connection to database
+        private bool OpenConnection()
+        {
+            try
+            {
+                connection.Open();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                //When handling errors, you can your application's response based 
+                //on the error number.
+                //The two most common error numbers when connecting are as follows:
+                //0: Cannot connect to server.
+                //1045: Invalid user name and/or password.
+                switch (ex.Number)
+                {
+                    case 0:
+                        Console.WriteLine("Cannot connect to server.  Contact administrator");
+                        break;
+
+                    case 1045:
+                        Console.WriteLine("Invalid username/password, please try again");
+                        break;
+                }
+                return false;
+            }
+        }
+
+        //Close connection
+        private bool CloseConnection()
+        {
+            try
+            {
+                connection.Close();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+        }
+
+        //Select statement
+        public List<string> Select(string query)
+        {
+            //Create a list to store the result
+            List<string> list = new List<string>();
+
+            //Open connection
+            if (this.OpenConnection() == true)
+            {
+                //Create Command
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+                //Create a data reader and Execute the command
+                MySqlDataReader dataReader = cmd.ExecuteReader();
+
+                //Read the data and store them in the list
+                while (dataReader.Read())
+                {
+                    list.Add(dataReader["id"] + "," + dataReader["datetime"] + "," + dataReader["api_temperature"]);
+                }
+
+                //close Data Reader
+                dataReader.Close();
+
+                //close Connection
+                this.CloseConnection();
+
+                //return list to be displayed
+                return list;
+            }
+            else
+            {
+                return list;
+            }
+        }
+
+        //Count statement
+        public int Count(string query)
+        {
+            int Count = -1;
+
+            //Open Connection
+            if (this.OpenConnection() == true)
+            {
+                //Create Mysql Command
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+
+                //ExecuteScalar will return one value
+                Count = int.Parse(cmd.ExecuteScalar() + "");
+
+                //close Connection
+                this.CloseConnection();
+
+                return Count;
+            }
+            else
+            {
+                return Count;
+            }
+        }
+    }
+}
---
+++ MySQLConnect/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Configuration;
+using System.Collections.Specialized;
+using System.Runtime.InteropServices;
+
+namespace MySQLConnect
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Database Connection Test!");
+
+            Connection con = new Connection();
+            Console.WriteLine("Lines found: " + con.Count("SELECT Count(*) FROM sensor_data"));
+            var result = con.Select("SELECT * FROM sensor_data");
+            foreach(var line in result)
+            {
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
