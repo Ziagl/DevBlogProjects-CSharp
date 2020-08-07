9d0f8a025df6f94d89ab47cf83962f2c147f4e87
Ziagl <office@ziegelwanger-edv.at>
2020-08-07 18:11:04 +0200

    added RaspberryPiGPIO

---
+++ RaspberryPiGPIO/GPIOControl.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Bifrost.Devices.Gpio;
+using Bifrost.Devices.Gpio.Abstractions;
+using Bifrost.Devices.Gpio.Core;
+
+namespace RaspberryPiGPIO
+{
+    class GPIOControl
+    {
+        IGpioController gpioController = GpioController.Instance;
+
+        public string GetPinStatus(int pinId)
+        {
+            GpioPinValue pinStatus;
+
+            Console.WriteLine("About to get pin status.");
+            var pin = gpioController.OpenPin(pinId);
+
+            pinStatus = pin.Read();
+
+            Console.WriteLine("Returning pin status.");
+            return pinStatus.ToString();
+        }
+
+    }
+}
---
+++ RaspberryPiGPIO/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace RaspberryPiGPIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pinId = 14;
+            GPIOControl control = new GPIOControl();
+
+            while(true)
+            {
+                Console.WriteLine("Pin Status: " + control.GetPinStatus(pinId));
+            }
+        }
+    }
+}
