91947151908509e9d316fca0d2edf5e6a6295747
Ziagl <office@ziegelwanger-edv.at>
2020-08-27 19:03:32 +0200

    added Docker support

--- PostgreSQLConnect/Startup.cs
+++ PostgreSQLConnect/Startup.cs
@@ -32,7 +32,7 @@ namespace PostgreSQLConnect
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
 
